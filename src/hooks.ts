783fa71db3df0c2a2cf72863487da1708347cb20
Bohdan Kucherivayi <bohdan.kucheriavyi@zapal.tech>
2025-01-29 01:07:55 +0200

    fix: turn off poc management

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,20 +1,21 @@
 import { createI18n } from '@inlang/paraglide-sveltekit'
 import type { Reroute } from '@sveltejs/kit'
 
-import { PUBLIC_WEB_APP_DOMAIN } from '$env/static/public'
+// import { PUBLIC_WEB_APP_DOMAIN } from '$env/static/public'
 import * as runtime from '$lib/paraglide/runtime'
-import { splitPath } from '$lib/utils'
+
+// import { splitPath } from '$lib/utils'
 
 // import { i18n } from '$lib/i18n'
 
 // Bypass i18n for /api routes
 export const reroute: Reroute = (event) => {
   if (event.url.pathname.startsWith('/api')) return
-  if (event.url.hostname === PUBLIC_WEB_APP_DOMAIN) {
-    const [localeOrSegment, ...rest] = splitPath(event.url.pathname)
+  // if (event.url.hostname === PUBLIC_WEB_APP_DOMAIN) {
+  //   const [localeOrSegment, ...rest] = splitPath(event.url.pathname)
 
-    event.url.pathname = `${['en', 'uk'].includes(localeOrSegment || '') ? `/${localeOrSegment}` : ''}/poc/${rest.join('/')}`
-  }
+  //   event.url.pathname = `${['en', 'uk'].includes(localeOrSegment || '') ? `/${localeOrSegment}` : ''}/poc/${rest.join('/')}`
+  // }
 
   return createI18n(runtime, { prefixDefaultLanguage: 'always' }).reroute()(event)
 }
