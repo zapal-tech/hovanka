a5ec5f003c43f9d373853e7b92401be8edb54a14
Bohdan Kucherivayi <bohdan.kucheriavyi@zapal.tech>
2025-01-29 13:13:50 +0200

    fix: svelte

--- svelte.config.js
+++ svelte.config.js
@@ -10,6 +10,9 @@ const config = {
   kit: {
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
+    prerender: {
+      handleHttpError: 'warn',
+    },
   },
 }
 
