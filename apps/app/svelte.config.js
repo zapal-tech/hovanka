84251dceb12bd938091a767b15af72d44004f412
Bohdan Kucherivayi <bohdan.kucheriavyi@zapal.tech>
2025-01-31 11:57:14 +0200

    fix: turn

--- apps/app/svelte.config.js
+++ apps/app/svelte.config.js
@@ -14,6 +14,7 @@ const config = {
   kit: {
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
+    csrf: false,
     alias: {
       '@api-types': path.resolve(__dirname, '../api/src/types/generated-types.ts'),
     },
