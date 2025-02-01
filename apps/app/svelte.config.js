2be010599cef00be15fdfb4954af3d59104a8a1b
Bohdan Kucherivayi <bohdan.kucheriavyi@zapal.tech>
2025-02-01 19:02:13 +0200

    fix: exclude

--- apps/app/svelte.config.js
+++ apps/app/svelte.config.js
@@ -15,7 +15,7 @@ const config = {
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter({
       routes: {
-        exclude: ['<all>'],
+        exclude: ['<build>'],
       },
       // fallback: 'spa',
       platformProxy: {
