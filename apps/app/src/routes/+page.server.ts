b1cb7a054dbcd20c4a6a2bcef5b4e0f8fce57fa5
Bohdan Kucherivayi <bohdan.kucheriavyi@zapal.tech>
2025-02-01 17:10:30 +0200

    fix: debug

--- apps/app/src/routes/+page.server.ts
+++ apps/app/src/routes/+page.server.ts
@@ -5,5 +5,11 @@ import { getMe } from '$lib/server/auth'
 export const load = async ({ cookies, request: { headers } }) => {
   const { user, token } = await getMe({ cookies, headers })
 
-  return { user, token, cookieVal: cookies.get(tokenCookieName), headers: Object.fromEntries(headers) }
+  return {
+    user,
+    token,
+    cookieName: tokenCookieName,
+    cookieVal: cookies.get(tokenCookieName),
+    headers: Object.fromEntries(headers),
+  }
 }
