afa3184d8f833ba6fabb955119aa30d12f90fcc9
Bohdan Kucherivayi <bohdan.kucheriavyi@zapal.tech>
2025-02-01 18:20:34 +0200

    fix: adapter setup

--- apps/app/svelte.config.js
+++ apps/app/svelte.config.js
@@ -13,7 +13,11 @@ const config = {
 
   kit: {
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter(),
+    adapter: adapter({
+      routes: {
+        exclude: ['<build>', '<files>'],
+      },
+    }),
     csrf: {
       checkOrigin: false,
     },
