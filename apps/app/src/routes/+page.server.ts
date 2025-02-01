cf88c4bf59f7c32791a5bb31e35771572495f4f8
Bohdan Kucherivayi <bohdan.kucheriavyi@zapal.tech>
2025-02-01 16:51:11 +0200

    fix: cookie

--- apps/app/src/routes/+page.server.ts
+++ apps/app/src/routes/+page.server.ts
@@ -1,7 +1,9 @@
+import { tokenCookieName } from '@hovanka/shared/cookies'
+
 import { getMe } from '$lib/server/auth'
 
 export const load = async ({ cookies, request: { headers } }) => {
   const { user, token } = await getMe({ cookies, headers })
 
-  return { user, token }
+  return { user, token, cookieVal: cookies.get(tokenCookieName) }
 }
--- apps/app/src/routes/+page.svelte
+++ apps/app/src/routes/+page.svelte
@@ -1,13 +1,10 @@
 <script lang="ts">
-  import { PUBLIC_API_URL } from '$env/static/public'
-
   import type { PageProps } from './$types'
 
   // Get props from page.server.ts
   let { data }: PageProps = $props()
 </script>
 
-<span>{PUBLIC_API_URL}</span>
 <span>{JSON.stringify(data)}</span>
 {#if data.user}
   <h1>Hi, {data.user.firstName}!</h1>
