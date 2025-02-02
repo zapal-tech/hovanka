4f517841a270568aa9b0e98c15bb8c25c18d1f74
Bohdan Kucherivayi <bohdan.kucheriavyi@zapal.tech>
2025-02-02 11:43:44 +0200

    fix: sign out button

--- apps/app/src/routes/(app)/settings/+page.svelte
+++ apps/app/src/routes/(app)/settings/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import { goto } from '$app/navigation'
   import Button from '$lib/components/button.svelte'
   import LanguageSwitcher from '$lib/components/language-switcher.svelte'
   import * as m from '$lib/paraglide/messages'
@@ -7,6 +6,6 @@
 
 <LanguageSwitcher />
 
-<Button class="mt-8" onclick={() => goto('/sign-out')}>
+<Button class="mt-8" href="/sign-out">
   {m.sign_out()}
 </Button>
