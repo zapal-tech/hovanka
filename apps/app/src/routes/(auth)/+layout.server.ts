81b33ec0f67dd748d7bbe322a7c23bc79b49842c
Bohdan Kucherivayi <bohdan.kucheriavyi@zapal.tech>
2025-02-02 02:09:19 +0200

    fix: layout

--- apps/app/src/routes/(auth)/+layout.server.ts
+++ apps/app/src/routes/(auth)/+layout.server.ts
@@ -3,5 +3,5 @@ import { redirect } from '@sveltejs/kit'
 import type { LayoutServerLoad } from './$types'
 
 export const load: LayoutServerLoad = async ({ locals, url }) => {
-  if (locals.user && url.pathname !== '/sign-out') redirect(307, '/')
+  if (locals.user && !url.pathname.endsWith('/sign-out')) redirect(307, '/')
 }
