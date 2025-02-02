654b1ded691a5c351120429793a056979a353688
Bohdan Kucherivayi <bohdan.kucheriavyi@zapal.tech>
2025-02-02 13:42:47 +0200

    fix: sign out

--- apps/app/src/routes/(auth)/sign-out/+server.ts
+++ apps/app/src/routes/(auth)/sign-out/+server.ts
@@ -1,7 +1,9 @@
 import { tokenCookieName } from '@hovanka/shared/cookies'
 
+import { PUBLIC_DOMAIN } from '$env/static/public'
+
 export const GET = async ({ cookies }) => {
-  cookies.delete(tokenCookieName, { path: '/' })
+  cookies.delete(tokenCookieName, { path: '/', domain: `.${PUBLIC_DOMAIN}` })
 
   return new Response(null, { status: 307, headers: { location: '/sign-in' } })
 }
