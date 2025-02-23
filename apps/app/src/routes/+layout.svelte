e9171bbb9861203c36028a39c32e98a61aa703c9
Bohdan Kucheriavyi <bohdan.kucheriavyi@zapal.tech>
2025-02-24 00:43:38 +0200

    fix: ga4 analytics script

--- apps/app/src/routes/+layout.svelte
+++ apps/app/src/routes/+layout.svelte
@@ -44,13 +44,16 @@
   <!-- Google tag (gtag.js) -->
   <script async src={`https://www.googletagmanager.com/gtag/js?id=${PUBLIC_GTAG_ID}`}></script>
   <script>
+    {
+      ;`
     window.dataLayer = window.dataLayer || []
     function gtag() {
       dataLayer.push(arguments)
     }
     gtag('js', new Date())
 
-    gtag('config', PUBLIC_GTAG_ID)
+    gtag('config', ${PUBLIC_GTAG_ID})`
+    }
   </script>
 
   {@html webManifestLink}
